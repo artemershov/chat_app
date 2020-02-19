9def3609922b4dd276e4441586770d77bc452556
Artem Ershov <artemershov1@gmail.com>
2020-02-19 08:13:04 +0200

    Chat component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Fragment,
+  useState,
+  useEffect,
+  useReducer,
+  useCallback
+} from 'react';
+import {
+  ChatBody,
+  ChatHeader,
+  ChatFooter,
+  ChatWindow,
+  ChatPromptNick
+} from './components';
+import { socketService } from './services';
+import uuidv4 from 'uuid/v4';
 
 function App() {
+  const [id, setID] = useState(null);
+  const [nick, setNick] = useState(null);
+
+  const [messages, dispatchMessages] = useReducer((state, action) => {
+    return action.type === 'message' ? [...state, action.message] : state;
+  }, []);
+
+  const addMessage = message => dispatchMessages({ type: 'message', message });
+
+  const sendMessage = useCallback(
+    text => {
+      const message = { id, nick, text, date: Date.now() };
+      addMessage(message);
+      socketService.emit('message', message);
+    },
+    [id, nick]
+  );
+
+  useEffect(() => {
+    if (!id) {
+      setID(uuidv4());
+    }
+    if (nick) {
+      const events = [
+        {
+          event: 'message',
+          callback: message => message.id !== id && addMessage(message)
+        }
+      ];
+      socketService.init(events);
+      return () => socketService.disconnect();
+    }
+  }, [id, nick]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatWindow>
+      {nick ? (
+        <Fragment>
+          <ChatHeader nick={nick} />
+          <ChatBody messages={messages} />
+          <ChatFooter onSubmit={sendMessage} />
+        </Fragment>
+      ) : (
+        <ChatPromptNick onSubmit={setNick} />
+      )}
+    </ChatWindow>
   );
 }
 
