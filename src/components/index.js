444d794b09b509a004700fc919ba9befd66cdd30
Artem Ershov <artemershov1@gmail.com>
2020-02-19 08:12:29 +0200

    components index

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as ChatBody} from './ChatBody';
+export {default as ChatBubbles} from './ChatBubbles';
+export {default as ChatFooter} from './ChatFooter';
+export {default as ChatHeader} from './ChatHeader';
+export {default as ChatWindow} from './ChatWindow';
+export {default as ChatPromptNick} from './ChatPromptNick';
